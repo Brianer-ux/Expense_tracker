5ed6f169c8840ccd9dd831482eb38ee7c8a5c009
Brianer <brianerngeno@gmail.com>
2025-12-29 18:15:55 +0300

    Adding authentication and user in model.py

--- expenses/models.py
+++ expenses/models.py
@@ -1,23 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
  
 class Category(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="categories"
+    )
+    name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
+
+
+    class Meta:
+        unique_together = ("user", "name")
+
 
     def __str__(self):
         return self.name
 
 
 class Expense(models.Model):
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="expenses"
+    )
     category = models.ForeignKey(
         Category,
         on_delete=models.CASCADE,
         related_name='expenses'
     )
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField()
     description = models.CharField(max_length=255, blank=True)
-
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
