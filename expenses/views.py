0b54182e12149336863bb18c8592480de3f690e9
Brianer <brianerngeno@gmail.com>
2025-12-30 18:47:33 +0300

    Add analytical endpoints for summary of expenses

--- expenses/views.py
+++ expenses/views.py
@@ -1,5 +1,8 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from django.db.models import Sum
 from .models import Expense, Category
 from .serializers import ExpenseSerializer, CategorySerializer
 
@@ -34,6 +37,47 @@ class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
     def get_queryset(self):
         return Category.objects.filter(user=self.request.user)
 
+class ExpenseSummaryView(APIView):
+    permission_classes = [IsAuthenticated]
 
+    def get(self, request):
+        period = request.query_params.get('period', 'monthly')
+
+        qs = Expense.objects.filter(user=request.user)
+
+        total = qs.aggregate(total_spent=Sum('amount'))['total_spent'] or 0
+
+        return Response({
+            "period": period,
+            "total_spent": total
+        })
+
+class CategoryBreakdownView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        data = (
+            Expense.objects
+            .filter(user=request.user)
+            .values('category__name')
+            .annotate(total=Sum('amount'))
+            .order_by('-total')
+        )
+
+        return Response(data)
+
+class TopCategoriesView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        data = (
+            Expense.objects
+            .filter(user=request.user)
+            .values('category__name')
+            .annotate(total=Sum('amount'))
+            .order_by('-total')[:5]
+        )
+
+        return Response(data)
 
 
